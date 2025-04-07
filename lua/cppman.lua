0f03e37e5d49d495e1e80c3e8b10f9bf5307cd40
GasparVardanyan <gaspar_pm@proton.me>
2025-04-08 02:36:07 +0400

    cword -> cWORD

--- lua/cppman.lua
+++ lua/cppman.lua
@@ -14,48 +14,48 @@ local stack = {}
 local current_page = nil
 
 local function reload(manwidth, word_to_search)
-    vim.bo.ro = false
-    vim.bo.ma = true
+	vim.bo.ro = false
+	vim.bo.ma = true
 
-    vim.api.nvim_buf_set_lines(0, 0, -1, true, {})
+	vim.api.nvim_buf_set_lines(0, 0, -1, true, {})
 
 	local cmd = string.format([[0r! cppman --force-columns %s '%s' ]], manwidth, word_to_search)
 	vim.cmd(cmd) -- Set buffer with cppman contents
 	vim.cmd("0") -- Go to top of document
-    
-    vim.bo.ro = true
-    vim.bo.ma = false
-    vim.bo.mod = false
 
-    vim.bo.keywordprg = "cppman"
-    vim.bo.buftype = "nofile"
+	vim.bo.ro = true
+	vim.bo.ma = false
+	vim.bo.mod = false
+
+	vim.bo.keywordprg = "cppman"
+	vim.bo.buftype = "nofile"
 	vim.bo.filetype = "cppman"
 end
 
 local function loadNewPage()
-    if current_page ~= nil then
-        table.insert(stack, current_page)
-    end
+	if current_page ~= nil then
+		table.insert(stack, current_page)
+	end
 
-    current_page = vim.fn.expand('<cword>')
+	current_page = vim.fn.expand('<cWORD>')
 
 	local wininfo = vim.fn.getwininfo(vim.fn.win_getid())[1]
 	local manwidth = wininfo.width - 4;
 
-    reload(manwidth, current_page)
+	reload(manwidth, current_page)
 end
 
 local function backToPrevPage()
-    if table.getn(stack) == 0 then 
-        return
-    end
+	if table.getn(stack) == 0 then
+		return
+	end
 
-    current_page = table.remove(stack)
+	current_page = table.remove(stack)
 
 	local wininfo = vim.fn.getwininfo(vim.fn.win_getid())[1]
 	local manwidth = wininfo.width - 4;
 
-    reload(manwidth, current_page)
+	reload(manwidth, current_page)
 end
 
 M.setup = function()
@@ -139,24 +139,24 @@ M.open_cppman_for = function(word_to_search)
 	popup:on(event.BufLeave, function()
 		popup:unmount()
 
-        current_page = nil
-        for i=0, #stack do stack[i]=nil end
+		current_page = nil
+		for i=0, #stack do stack[i]=nil end
 	end)
 
 	-- Set content
 	local wininfo = vim.fn.getwininfo(popup.winid)[1]
 	local manwidth = wininfo.width - 4;
-    
-    reload(manwidth, word_to_search)
 
-    current_page = word_to_search
+	reload(manwidth, word_to_search)
+
+	current_page = word_to_search
 
 	vim.keymap.set("n", "q", ":q!<cr>", { silent = true, buffer = true })
 
 	vim.keymap.set("n", "K", loadNewPage, { silent = true, buffer = true })
 	vim.keymap.set("n", "<C-]>", loadNewPage, { silent = true, buffer = true })
 	vim.keymap.set("n", "<2-LeftMouse>", loadNewPage, { silent = true, buffer = true })
-    
+
 
 	vim.keymap.set("n", "<C-T>", backToPrevPage, { silent = true, buffer = true })
 	vim.keymap.set("n", "<RightMouse>", backToPrevPage, { silent = true, buffer = true })
